c7d14cda898e7d449f1b17fe58500bdf1ed01241
Andrew Perrault <andrew.perrault@yahoo.ca>
2014-08-20 12:10:56 -0400

    Chapter 6 exercises started

---
+++ chapter6/exercise1/exercise1/Source.cpp
@@ -0,0 +1,17 @@
+// Mad-Lib
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+string askText(string prompt);
+int askNumber(string prompt);
+void tellStory(string name, string noun, int number, string bodyPart, string verb);
+
+int main()
+{
+	cout << "Welcome to Mad Lib.\n\n";
+	
+	return 0;
+}
